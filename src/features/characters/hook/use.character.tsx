0660be1cea925e8db50d13252e5507e5c50f1a80
rocioRichi <paragit@gmail.com>
2023-02-21 23:12:10 +0100

    Add hook hook

--- src/features/characters/hook/use.character.tsx
+++ src/features/characters/hook/use.character.tsx
@@ -1,1 +1,31 @@
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../../core/store/store";
+import { useEffect } from "react";
+import { AppDispatch } from "../../../core/store/store";
+import { CharacterApiRepo } from "../../../repo/character.api.repo";
+import { loadCreator } from "../reducer/character.actions.creater";
 
+export function useTasks(repo: CharacterApiRepo) {
+  const tasks = useSelector((state: RootState) => state.character);
+  const dispatch = useDispatch<AppDispatch>();
+
+  useEffect(() => {
+    const loadTasks = async () => {
+      try {
+        const data = await repo.loadCharacter();
+        dispatch(loadCreator(data));
+      } catch (error) {
+        console.log((error as Error).message);
+      }
+    };
+    loadCharacter();
+  }, [dispatch, repo]);
+
+  return {
+    tasks,
+  };
+}
+
+function loadCharacter() {
+  throw new Error("Function not implemented.");
+}
