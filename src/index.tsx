f2f3cdab25b014ae7f5cc016de27eae10a67c89f
rocioRichi <paragit@gmail.com>
2023-02-22 07:29:02 +0100

    Remove import from app

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./core/store/store";
-import App from "./core/components/app/App";
 import reportWebVitals from "./reportWebVitals";
 import "./index.css";
 
